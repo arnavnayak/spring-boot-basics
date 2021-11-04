6126041897b7990f186c8e1e55720fe11c5545a9
Arnav <neilmanoj@gmail.com>
2021-11-04 13:29:04 +0530

    pushing spring boot changes

---
+++ src/main/java/com/practice/spring/basics/springbasics/basic/BinarySearchImprovedWithoutSpringBoot.java
@@ -0,0 +1,41 @@
+package com.practice.spring.basics.springbasics.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class BinarySearchImprovedWithoutSpringBoot {
+
+//	private Logger logger = LoggerFactory.getLogger(BinarySearchImprovedWithoutSpringBoot.class);
+	@Autowired
+	@Qualifier("quick")
+	SortAlgo sortAlgo; // instead of sortAlgo we can use the implmentation class name "bubbleSortAlgo" ahere so as to specify which 
+						// implemntation to inject between quick sort and bubble sort doing this we can avoid using
+							// @Primary
+
+	/*
+	 * public void setSortAlgo(SortAlgo sortAlgo) { // setter injection  // not even required to create it will take it automatically
+	 * this.sortAlgo = sortAlgo; }
+	 */
+	
+	public int binarySearch(int[] numbers,int numberToSearch) {
+		int[] sortedNumbers = sortAlgo.sort(numbers);
+//		logger.info("{}",sortAlgo);
+		return 3;
+	}
+
+	/*@PostConstruct
+	public void postConstruct(){
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy
+	public void PreDestroy(){
+		logger.info("PreDestroy");
+	}*/
+	
+}
